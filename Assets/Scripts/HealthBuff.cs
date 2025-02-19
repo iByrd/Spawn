61159e744fc9a44d543f627b8ce09a12281d6a8f
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2025-02-19 12:57:21 -0500

    add healthbuff logic and asset

---
+++ Assets/Scripts/HealthBuff.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerups/HealthBuff")]
+public class HealthBuff : PowerupEffect
+{
+    public float amount;
+    public override void Apply(GameObject target)
+    {
+
+        target.GetComponent<PlayerHealth>().health += amount;
+    }
+}
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public PowerupEffect powerupEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        powerupEffect.Apply(collision.gameObject);
+    }
+}
